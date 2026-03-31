66298ad78821912eee2208f1bb57d98038c8661a
kenyafernandes <kenya.fernandes@sydney.edu.au>
2026-03-31 23:58:08 +0000

    Deploying to gh-pages from @ kenyafernandes/kenyafernandes.github.io@cb94f61e7fd03cb0af21d5fe58c00a19464e0e3c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -78,13 +78,13 @@ ninja.data = [{
       },
     },{id: "post-australian-stingless-bee-honey-packs-a-serious-antimicrobial-punch",
       
-        title: "Australian Stingless Bee Honey Packs A Serious Antimicrobial Punch",
+        title: 'Australian stingless bee honey packs a serious antimicrobial punch! <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
-      description: "",
+      description: "An article for The Amateur Beekeeper newsletter.",
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2026/australian-stingless-bee-honey-packs-a-serious-antimicrobial-punch/";
+          window.open("https://tab.beekeepers.asn.au/issue-january-2026/native-bees", "_blank");
         
       },
     },{id: "post-stingless-bee-honey-shows-unique-medicinal-potential",
