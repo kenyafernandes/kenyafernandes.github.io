258517c7edd6b9e3577171c4c2b6802cfe1dec33
kenyafernandes <kenya.fernandes@sydney.edu.au>
2026-03-31 23:52:19 +0000

    Deploying to gh-pages from @ kenyafernandes/kenyafernandes.github.io@ad8c0d42503d66ba37430dd30fb100a15cba0e00 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,7 +65,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "post-stingless-bee-honey-shows-unique-medicinal-potential",
+        },{id: "post-honey-from-australian-wildflowers-has-potent-power-to-kill-bacteria",
+      
+        title: "Honey From Australian Wildflowers Has Potent Power To Kill Bacteria",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2026/honey-from-australian-wildflowers-has-potent-power-to-kill-bacteria/";
+        
+      },
+    },{id: "post-stingless-bee-honey-shows-unique-medicinal-potential",
       
         title: 'Stingless bee honey shows unique medicinal potential <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
