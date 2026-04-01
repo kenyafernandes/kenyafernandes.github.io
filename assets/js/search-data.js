ed1885d921afbb071f6e1d6a83886d47d85ca87c
kenyafernandes <kenya.fernandes@sydney.edu.au>
2026-04-01 00:08:07 +0000

    Deploying to gh-pages from @ kenyafernandes/kenyafernandes.github.io@b0a1f021eadc00c55c696ffb4fd7640e43290184 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -67,13 +67,13 @@ ninja.data = [{
           },
         },{id: "post-honey-from-australian-wildflowers-has-potent-power-to-kill-bacteria",
       
-        title: "Honey From Australian Wildflowers Has Potent Power To Kill Bacteria",
+        title: 'Honey from Australian wildflowers has potent power to kill bacteria <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
-      description: "",
+      description: "An article for The Conversation.",
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2026/honey-from-australian-wildflowers-has-potent-power-to-kill-bacteria/";
+          window.open("https://theconversation.com/honey-from-australian-wildflowers-has-potent-power-to-kill-bacteria-276630", "_blank");
         
       },
     },{id: "post-australian-stingless-bee-honey-packs-a-serious-antimicrobial-punch",
